89a872f3b87c0defe2b85758006a21d1fb9ee0f8
gremoryyx <arispelgremory@gmail.com>
2023-01-01 10:37:42 +0800

    Added both legs 2

---
+++ Robot/Robot/Leg.cpp
@@ -0,0 +1,6 @@
+
+void Legs()
+{
+    // LeftFeet();
+}
+
--- Robot/Robot/Robot.cpp
+++ Robot/Robot/Robot.cpp
@@ -3,13 +3,22 @@
 #include <Windows.h>
 #include <gl/GL.h>
 #include <gl/GLU.h>
+// #include <GL/glut.h>
 
 #pragma comment (lib, "OpenGL32.lib")
 #pragma comment (lib, "glu32.lib")
 
 #define WINDOW_TITLE "Robot"
 
-float angle = 0.0f;
+float angle = 0.0f, rotationX, rotationY, rotationZ;
+float ONear = -10.0, OFar = 10.0;
+float PNear = 1.0, PFar = 20.0;
+float Ptx = 0.0 , Pty = 0.0 , PtSpeed = 1.0;
+float Pry =0.0 , PrSpeed=1.0;
+
+bool isOrtho = true;
+float tx = 0.0, ty = 0.0, tz = 0.0, tSpeed = 1.0;
+float rS = 0.0,rSpeed = 1.0;
 
 LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -21,8 +30,93 @@ LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam
 
 		case WM_KEYDOWN:
 			if (wParam == VK_ESCAPE) PostQuitMessage(0);
-			else if (wParam == VK_LEFT) angle -= 0.5f;
-			else if (wParam == VK_RIGHT) angle += 0.5f;
+			else if (wParam == VK_LEFT) angle -= 1.0f;
+			else if (wParam == VK_RIGHT) angle += 1.0f;
+			else if (wParam == 'X')
+			{
+				rotationX = 1.0f;
+				rotationY = 0.0f;
+				rotationZ = 0.0f;
+			} else if (wParam == 'Y')
+			{
+				rotationY = 1.0f;
+				rotationX = 0.0f;
+				rotationZ = 0.0f;
+			} else if (wParam == 'Z')
+			{
+				rotationZ = 1.0f;
+				rotationX = 0.0f;
+				rotationY = 0.0f;
+			} else if (wParam == VK_SPACE)
+			{
+				rotationY = 1.0;
+				rotationX = 0.0;
+				rotationZ = 0.0;
+				angle = 0.0;
+				glLoadIdentity();
+			}
+			else if (wParam == VK_UP) {
+				if (isOrtho){
+					if (tz > ONear)
+						tz -= tSpeed;
+				}
+				else {
+					if (tz > PNear)
+						tz -= tSpeed;
+				}
+			}
+			else if (wParam == VK_DOWN) {
+				if (isOrtho) {
+					if (tz < OFar)
+						tz += tSpeed;
+				}
+				else {
+					if (tz < PFar)
+						tz += tSpeed;
+				}
+			}
+			else if (wParam == VK_LEFT) {
+				if (isOrtho) {
+					if (tx > ONear)
+						tx -= tSpeed;
+				}
+				else {
+					if (tx > PNear)
+						tx -= tSpeed;
+				}
+			}
+			else if (wParam == VK_RIGHT) {
+				if (isOrtho) {
+					if (tx < OFar)
+						tx += tSpeed;
+				}
+				else {
+					if (tx < PFar)
+						tx += tSpeed;
+				}
+			}
+			else if (wParam == 'A') {
+				Ptx -= PtSpeed;
+			}
+			else if (wParam == 'D') {
+				Ptx += PtSpeed;
+			}
+			else if (wParam == 'L') {
+				Pry += PrSpeed;
+			}
+			else if (wParam == 'R') {
+				Pry -= PrSpeed;
+			}
+			else if (wParam == 0x5A) //Z
+				{
+				isOrtho = false;
+				tz = PNear;
+				}
+		else if (wParam == 0x58) //X
+			{
+			isOrtho = true;
+			}
+			
 		break;
 
 		default:
@@ -77,48 +171,44 @@ void rgb(int r, int g, int b)
 }
 
 // Trapezium function
-void DrawTriangularPrism()
+void DrawTriangularPrism(float x, float y, float z, float height, float width, float depth, GLenum shape)
 {
 	// Front
-	glBegin(GL_LINE_LOOP);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.5f, 0.0f, 0.0f);
-	glVertex3f(0.5f, 0.5f, 0.5f);
-	glVertex3f(0.0f, 0.5f, 0.5f);
+	glBegin(shape);
+	glVertex3f(x, y, z);
+	glVertex3f(x + width, y, z);
+	glVertex3f(x + width, y + height, z + depth);
+	glVertex3f(x, y + height, z + depth);
 	glEnd();
 	
 	// Left
-	glBegin(GL_LINE_LOOP);
-	rgb(255, 255, 255);
-	glVertex3f(0.0f, 0.5f, 0.5f);
-	glVertex3f(0.0f, 0.0f, 0.5f);
-	glVertex3f(0.0f, 0.0f, 0.0f);
+	glBegin(shape);
+	glVertex3f(x, y + height, z + depth);
+	glVertex3f(x, y, z + depth);
+	glVertex3f(x, y, z);
 	glEnd();
 
 	// Bottom
-	glBegin(GL_LINE_LOOP);
-	rgb(0, 255, 0);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.0f, 0.5f);
-	glVertex3f(0.5f, 0.0f, 0.5f);
-	glVertex3f(0.5f, 0.0f, 0.0f);
+	glBegin(shape);
+	glVertex3f(x, y, z);
+	glVertex3f(x, y, z + depth);
+	glVertex3f(x + width, y, z + depth);
+	glVertex3f(x + width, y, z);
 	glEnd();
 
 	// Right
-	glBegin(GL_LINE_LOOP);
-	rgb(0, 0, 255);
-	glVertex3f(0.5f, 0.0f, 0.0f);
-	glVertex3f(0.5f, 0.0f, 0.5f);
-	glVertex3f(0.5f, 0.5f, 0.5f);
+	glBegin(shape);
+	glVertex3f(x + width, y, z);
+	glVertex3f(x + width, y, z + depth);
+	glVertex3f(x + width, y + height, z + depth);
 	glEnd();
 
 	// Back
-	glBegin(GL_LINE_LOOP);
-	rgb(255, 255, 0);
-	glVertex3f(0.5f, 0.5f, 0.5f);
-	glVertex3f(0.0f, 0.5f, 0.5f);
-	glVertex3f(0.0f, 0.0f, 0.5f);
-	glVertex3f(0.5f, 0.0f, 0.5f);
+	glBegin(shape);
+	glVertex3f(x + width, y + height, z + depth);
+	glVertex3f(x, y + height, z + depth);
+	glVertex3f(x, y, z + depth);
+	glVertex3f(x + width, y, z + depth);
 	glEnd();
 }
 
@@ -128,7 +218,6 @@ void DrawCube(float x, float y, float z, float height, float width, float depth,
 {
 	// Front face
 	glBegin(shape);
-	rgb(255, 255, 255);
 	glVertex3f(x, y, z);
 	glVertex3f(x, y, z + depth);
 	glVertex3f(x, y + height, z + depth);
@@ -137,7 +226,6 @@ void DrawCube(float x, float y, float z, float height, float width, float depth,
 
 	// Right face
 	glBegin(shape);
-	// rgb(0, 255, 0);
 	glVertex3f(x, y, z);
 	glVertex3f(x + width, y, z);
 	glVertex3f(x + width, y, z + depth);
@@ -146,7 +234,6 @@ void DrawCube(float x, float y, float z, float height, float width, float depth,
 
 	// Back face
 	glBegin(shape);
-	// rgb(0,0,255);
 	glVertex3f(x + width, y, z);
 	glVertex3f(x + width, y + height, z);
 	glVertex3f(x + width, y + height, z + depth);
@@ -155,7 +242,6 @@ void DrawCube(float x, float y, float z, float height, float width, float depth,
 
 	// Left face
 	glBegin(shape);
-	// rgb(0, 255, 0);
 	glVertex3f(x + width, y + height, z);
 	glVertex3f(x, y + height, z);
 	glVertex3f(x, y + height, z + depth);
@@ -164,7 +250,6 @@ void DrawCube(float x, float y, float z, float height, float width, float depth,
 
 	// Top face
 	glBegin(shape);
-	rgb(255, 255, 0);
 	glVertex3f(x, y + height, z + depth);
 	glVertex3f(x + width, y + height, z + depth);
 	glVertex3f(x + width, y, z + depth);
@@ -173,7 +258,6 @@ void DrawCube(float x, float y, float z, float height, float width, float depth,
 
 	// Bottom face
 	glBegin(shape);
-	// rgb(0, 255, 255);
 	glVertex3f(x, y, z);
 	glVertex3f(x + width, y, z);
 	glVertex3f(x + width, y + height, z);
@@ -234,6 +318,7 @@ void DrawCylinder(float topRadius, float baseRadius, float height, int slices, i
 	GLUquadricObj* cylinder = NULL;
 	cylinder = gluNewQuadric();
 	gluQuadricDrawStyle(cylinder, GLU_FILL);
+	// GLUQuadricLineWidth(cylinder, 2.0);
 	gluCylinder(cylinder, baseRadius, topRadius, height, slices, stacks);
 	gluDeleteQuadric(cylinder);
 }
@@ -247,6 +332,79 @@ void DrawCone(int baseRadius, float height, int slices, int stacks)
 	gluDeleteQuadric(cone);
 }
 
+void DrawRightFoot(float centerX, float centerY, float centerZ)
+{
+	// Toes
+	rgb(255,255,255);
+	
+	glPushMatrix();
+		glTranslatef(centerX, centerY, centerZ);
+		DrawTriangularPrism(0,0,0, 0.5,1.0,0.5,GL_LINE_LOOP);
+	glPopMatrix();
+	
+	DrawCube(centerX, centerY, 0.55, 0.5, 1.0, 0.5, GL_LINE_LOOP);
+	// left part 
+	DrawCube(centerX + 1.0, centerY, 0.5, 0.7, 0.2, 0.80, GL_LINE_LOOP);
+	// Right part (More thin)
+	DrawCube(centerX + -0.1, centerY, 0.5, 0.7, 0.1, 0.80, GL_LINE_LOOP);
+	// Behind part
+	DrawCube( centerX,centerY, 1.0, 0.7, 1.0, 0.3, GL_LINE_LOOP);
+	
+	// Ankle
+	rgb(0,0,255);
+	DrawCube(centerX + -0.1,centerY + 0.7, 0.5, 3.0, 1.3, 0.8, GL_LINE_LOOP);
+	// Left Leg, Tyre at left side
+	rgb(255,255,0);
+	glPushMatrix();
+		
+		// Lower Tyre
+		glTranslatef(centerX + 1.0, centerY + 1.2, 0.7);
+		glRotatef(90, 0, 1, 0);
+		DrawCylinder(0.3, 0.3, 0.2, 20, 20);
+		glPushMatrix();
+			// Upper Tyre
+			glTranslatef(0, 0.7, 0);
+			DrawCylinder(0.3, 0.3, 0.2, 20, 20);
+		glPopMatrix();
+	glPopMatrix();
+}
+
+void DrawLeftFoot(float centerX, float centerY, float centerZ)
+{
+	// Toes
+	rgb(255,255,255);
+	
+	glPushMatrix();
+	glTranslatef(centerX, centerY, centerZ);
+	DrawTriangularPrism(0,0,0, 0.5,1.0,0.5,GL_LINE_LOOP);
+	glPopMatrix();
+	
+	DrawCube(centerX, centerY, 0.55, 0.5, 1.0, 0.5, GL_LINE_LOOP);
+	// left part (More thin)
+	DrawCube(centerX - 0.1,centerY, 0.5, 0.7, 0.1, 0.80, GL_LINE_LOOP);
+	// Right part 
+	DrawCube(centerX + 1.0,centerY, 0.5, 0.7, 0.2, 0.80, GL_LINE_LOOP);
+	// Behind part
+	DrawCube( centerX,centerY, 1.0, 0.7, 1.0, 0.3, GL_LINE_LOOP);
+	
+	// Ankle
+	rgb(0,0,255);
+	DrawCube(centerX + -0.1,centerY + 0.7, 0.5, 3.0, 1.3, 0.8, GL_LINE_LOOP);
+	
+	// Right Leg, Tyre at Right side
+	rgb(255,255,0);
+	glPushMatrix();
+		// Lower Tyre
+		glTranslatef(centerX - 0.1, centerY + 1.2, 0.7);
+		glRotatef(90, 0, 1, 0);
+		DrawCylinder(0.3, 0.3, 0.2, 20, 20);
+		glPushMatrix();
+		// Upper Tyre
+			glTranslatef(0, 0.7, 0);
+			DrawCylinder(0.3, 0.3, 0.2, 20, 20);
+		glPopMatrix();
+	glPopMatrix();
+}
 
 void display()
 {
@@ -257,17 +415,16 @@ void display()
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	// glMatrixMode(GL_MODELVIEW);
-
-	angle = 0.1f;
+	
 	if (angle > 360.0f)
 	{
 		angle = 0.0f;
 	}
 	
-	glRotatef(angle, 0.0f, 1.0f, 0.0f);
+	glRotatef(angle, rotationX, rotationY, rotationZ);
 	std::cout << "Angle: " << angle << std::endl;
-	// DrawTriangularPrism();
-
+	DrawLeftFoot(-1.5f, -5.0f, 0.0f);
+	DrawRightFoot(1.5f, -5.0f, 0.0f);
 	
 	
 	//--------------------------------
@@ -275,6 +432,23 @@ void display()
 	//--------------------------------
 }
 //--------------------------------------------------------------------
+
+void projection() {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glTranslatef(Ptx, Pty, 0.0);
+	glRotatef(Pry, 0.0, 1.0, 0.0);
+	if (isOrtho) {
+		//ortho view
+		glOrtho(-10.0,10.0, -10.0,10.0,ONear,OFar);
+	}
+	else{
+		//perspective view
+		gluPerspective(20.0, 1.0, -1.0, 1.0);
+		glFrustum(-10.0, 10.0, -10.0, 10.0, PNear, PFar);
+	}
+}
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow)
 {
@@ -327,6 +501,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow)
 			DispatchMessage(&msg);
 		}
 
+		projection();
 		display();
 		
 		SwapBuffers(hdc);
